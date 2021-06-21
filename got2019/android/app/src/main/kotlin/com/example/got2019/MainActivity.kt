02bd3fec86b39b64479bdfdafd3bce70515ea9ea
Infinite-eyes <cwm930215@gmail.com>
2021-06-21 11:32:40 +0800

    1.add got  page

---
+++ got2019/android/app/src/main/kotlin/com/example/got2019/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.got2019
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ got2019/lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+import 'got_page.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primaryColor: Colors.amber,
+        accentColor: Colors.black,
+        fontFamily: "Pacifico",
+      ),
+      debugShowCheckedModeBanner: false,
+      home: GotPage(),
+    );
+  }
+}
