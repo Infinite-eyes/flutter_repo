8e7aa8a05d28e959b24a18618b4eba3b79f2e44d
Infinite-eyes <cwm930215@gmail.com>
2021-07-09 19:51:49 +0800

    1.add stack over flow app

---
+++ stack_over_flow_app/lib/home_provider.dart
@@ -0,0 +1,20 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:stack_over_flow_app/home_model.dart';
+
+class HomeProvider {
+  HomeProvider();
+
+  Future<QuestionData> getData() async {
+    var res = await http.get("http://192.168.0.141:8000/stack/questions/?format=json");
+    var decodedJson = jsonDecode(res.body);
+
+    QuestionData qd  = QuestionData();
+    qd.questions = [];
+    for(var json in decodedJson){
+      qd.questions.add(Questions.fromJson(json));
+    }
+    return qd;
+  }
+}
